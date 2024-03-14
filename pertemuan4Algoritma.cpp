34270fadff8f45a8a574aecd36a73fe5f37e5a39
muhammadrayhanwidayat <murayat99@gmail.com>
2024-03-14 10:27:09 +0700

    menambahkan tampilan susunan data elemen array dan memberikan perulangan for

--- pertemuan4Algoritma.cpp
+++ pertemuan4Algoritma.cpp
@@ -17,7 +17,17 @@ void input() {			//prosedure input
 			cout << "\nArray yang ada masukan maksimal 20 elemen.\n";//menampilkan pesan jika
 		}
 	}
-	cout << endl;
+	cout << endl;			//membuat jarak per baris program
+	cout << "===================" << endl;	//membuat tampilan susunan data elemen array
+	cout << "masukan elemen array" << endl;
+	cout << "===================" << endl;
+
+	for (int i = 0; i < n; i++)				//menggunakan perulangan for untuk menyiapkan data pada array
+	{
+		cout << "data ke-" << (i + 1) << ":";	//memasukan atau menginputkan for untuk menyiapkan data pada n
+		cin >> arr[i];							//menyiapkan nilai data n kedalam array arr
+	}
+
 }
 
 int main()
