121ef9e73e6eef5b3092c4327f70fad585482d67
muhammadrayhanwidayat <murayat99@gmail.com>
2024-03-14 10:19:14 +0700

    menambahkan prosedur input

--- pertemuan4Algoritma.cpp
+++ pertemuan4Algoritma.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];			
-int n;
+int arr[20];			//membuat array dengan panjang data 20
+int n;					//membuat variabel inputan n
 
+
+void input() {			//prosedure input
+	while (true)
+	{
+		cout << "masukan jumlah Data pada array:";		//membuat inputan jumlah elemen array
+		cin >> n;										//memanggil variable inputan n
+		if (n <= 20) {
+			break;
+		}
+		else {
+			cout << "\nArray yang ada masukan maksimal 20 elemen.\n";//menampilkan pesan jika
+		}
+	}
+	cout << endl;
+}
 
 int main()
 {
